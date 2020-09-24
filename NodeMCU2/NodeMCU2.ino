0c05d14e158e7d422c6a9668a7d41c50076e168a
Evgeny Gribanov <Gribanoveu@yandex.ru>
2020-09-24 12:40:36 +0400

    Update NodeMCU2.ino
    Add ElegantOTA

--- NodeMCU2/NodeMCU2.ino
+++ NodeMCU2/NodeMCU2.ino
@@ -1,3 +1,9 @@
+// Elegant OTA UPDATES
+#include <elegantWebpage.h>
+#include <ElegantOTA.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
+
 // Blynk
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
@@ -19,6 +25,8 @@ char auth[] = "lP8CXFCFmjsxNnQ93EBUBUoBlnh9VmP5";
 // Your WiFi credentials.
 char ssid[] = "CoffeLink";
 char pass[] = "qqww1122";
+ESP8266WebServer server(80);
+
 
 void setup()
 {
@@ -26,12 +34,36 @@ void setup()
   Blynk.begin(auth, ssid, pass); // run blynk
   tempSensors.begin(); // run DS18B20 sensors
   timer.setInterval(2000L, getSendData); // run blynk timer update 2 second
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "Hi! I am ESP8266.");
+  });
+  
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop()
 {
   timer.run();
   Blynk.run();
+  server.handleClient();
 }
 
 void getSendData() 
