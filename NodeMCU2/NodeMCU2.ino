20c29a7dfe0b59fd4da2f326a61148f7daf043d2
Evgeny Gribanov <Gribanoveu@yandex.ru>
2020-09-24 12:06:30 +0400

    Create NodeMCU2.ino
    Add temp sensor DS18B20

---
+++ NodeMCU2/NodeMCU2.ino
@@ -0,0 +1,51 @@
+// Blynk
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+BlynkTimer timer;
+
+// Temp 
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#define ONE_WIRE_BUS 4  //D2 pin of nodemcu
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature tempSensors(&oneWire);  
+int temp_0; // переменная для 1 датчика
+int temp_1; // переменная для 2 датчика
+
+// Blynk token
+char auth[] = "lP8CXFCFmjsxNnQ93EBUBUoBlnh9VmP5";
+
+// Your WiFi credentials.
+char ssid[] = "CoffeLink";
+char pass[] = "qqww1122";
+
+void setup()
+{
+  Serial.begin(115200); // Debug console
+  Blynk.begin(auth, ssid, pass); // run blynk
+  tempSensors.begin(); // run DS18B20 sensors
+  timer.setInterval(2000L, getSendData); // run blynk timer update 2 second
+}
+
+void loop()
+{
+  timer.run();
+  Blynk.run();
+}
+
+void getSendData() 
+{
+  tempSensors.requestTemperatures(); 
+  temp_0 = tempSensors.getTempCByIndex(0); // Sensor 0 показания для датчика 1 в цельсиях
+  temp_1 = tempSensors.getTempCByIndex(1); // Sensor 0 показания для датчика 2 в цельсиях 
+
+  Serial.print("Temp_0: ");
+  Serial.print(temp_0);
+  Serial.print(" oC . Temp_1: ");
+  Serial.print(temp_1);
+  Serial.println(" oC");
+   
+  Blynk.virtualWrite(10, temp_0); //выврд данных на виртуальный пин V10
+  Blynk.virtualWrite(11, temp_1); //вывод данных навиртуальный пин V11
+}
